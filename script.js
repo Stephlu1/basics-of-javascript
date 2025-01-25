e68b8708a49fe979fabdce06a1ea5d8016da80f9
Stephlu1 <yoursassycow@gmail.com>
2025-01-24 23:38:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Declare and Use Variables
+let StudentName = "Luanna Stephens";
+let StudentAge = 27;
+let isEnrolled = true;
+
+// Log the variables to the console
+console.log(StudentName); 
+console.log(StudentAge); 
+console.log(isEnrolled); 
+
+// Demonstrate Data Types
+let FirstName = "Luanna" // String Value
+console.log(FirstName)
+
+let height = 167.64; // Number Value
+console.log(height); 
+
+let isHuman = true; // Boolean Value
+console.log(isHuman)
+
+let Pokemon = ['Turtwig', 'Snivy', 'Bulbasaur']; // Array Value
+console.log(Pokemon); 
+
+let birthDay = {
+   month: "October",
+   day: 16,
+   year: 1997,
+ }; // Object Value
+console.log(birthDay); 
+
+// Arithmetic Operators
+console.log(32 + 5); 
+console.log(4 - 1); 
+console.log(6 * 9); 
+console.log(8 / 2); 
+
+// Comparison Operators
+let x = 3
+let y = 7
+console.log(x > y); 
+console.log(x < y); 
+console.log (x === y); 
+console.log (x != y); 
+
+// Logical Operators
+let a = true;
+let b = false;
+console.log(a && b); 
+console.log(a || b); 
+console.log(!a); 
